a6398598834715976b86251a13eb091ad87a224f
incolorate <88613908+incolorate@users.noreply.github.com>
2023-03-08 10:17:07 +0200

    delete/edit/create functions edited to work with local db

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import axios from "axios";
@@ -6,24 +6,28 @@ import axios from "axios";
 function App() {
   const [books, setBooks] = useState([]);
 
+  // Load books from DB;
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await axios.get(`http://localhost:3000/books`);
+      setBooks(response.data);
+      console.log(response.data);
+    };
+    getBooks();
+  }, []);
+
   // create a new book object
   const createBook = async (title) => {
     const response = await axios.post(`http://localhost:3000/books`, {
       title,
     });
-    console.log(response);
-    // setBooks((prevBooks) => [
-    //   ...prevBooks,
-    //   {
-    //     id: nanoid(),
-    //     title: title,
-    //   },
-    // ]);
+    setBooks((prevBooks) => [...prevBooks, response.data]);
   };
 
   // delete a book
+  const deleteBook = async (id) => {
+    const response = await axios.delete(`http://localhost:3000/books/${id}`);
 
-  const deleteBook = (id) => {
     setBooks((prevBooks) => {
       return prevBooks.filter((book) => {
         return book.id !== id;
@@ -32,12 +36,16 @@ function App() {
   };
 
   // New title
-  const editBook = (id, title) => {
-    setBooks((prevBooks) => {
-      return prevBooks.map((book) => {
-        return book.id === id ? { ...book, title: title } : book;
-      });
+  const editBook = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3000/books/${id}`, {
+      title: newTitle,
     });
+    // Update the book list
+    setBooks((prevBooks) =>
+      prevBooks.map((book) => {
+        return book.id === id ? response.data : book;
+      })
+    );
   };
 
   return (
